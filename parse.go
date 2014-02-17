b561d98b80b7dca56a111b21c5d7633606a11a0b
dickeyxxx <jeff@dickeyxxx.com>
2014-02-17 11:07:16 -0800

    documentation!

--- parse.go
+++ parse.go
@@ -1,11 +1,15 @@
-package github_markdown
+// provides a function to parse markdown via GitHub's API
+// http://developer.github.com/v3/markdown/
+package githubmarkdown
 
 import (
-	"strings"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
+// Parses markdown with an HTTP request to GitHub's api.
+// It retuns an html string.
 func Parse(md string) (string, error) {
 	reader := strings.NewReader(md)
 	resp, err := http.Post("https://api.github.com/markdown/raw", "text/x-markdown", reader)
